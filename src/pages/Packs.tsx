999db228f90380a442e21a361e7b2c95e77b2aca
artem_vasiliev <vincere802@gmail.com>
2020-08-12 16:53:50 +0300

    Зарефакторил таблицу, сделал отдельной компонентой.

--- src/pages/Packs.tsx
+++ src/pages/Packs.tsx
@@ -1,47 +1,47 @@
 import * as React from 'react';
-import { useCallback, useEffect} from "react";
-import {addPack, deletePack, getPacks,updatePack} from "../redux/reducers/packReducer";
+import {useCallback, useEffect} from "react";
+import {addPack, deletePack, getPacks, updatePack} from "../redux/reducers/packReducer";
 import {useDispatch, useSelector} from "react-redux";
 import {AppStateType} from "../redux/store";
-import Pack from '../components/Pack';
 import AddPack from "../components/AddPack";
-import { action } from '../redux/actions/login';
+import {action} from '../redux/actions/login';
 import {SearchFilter} from "../components/SearchFilter";
 import {actionPack} from "../redux/actions/packs";
 import Pagination from "react-js-pagination";
-import '../scss/packs.scss'
+
+import {Table} from "../components/Table";
 
 
 type Props = {};
 
 const Packs = React.memo((props: Props) => {
     const dispatch = useDispatch()
-    const {cardPacks,currentPage,pageCount,cardPackTotalCount} = useSelector(({packs}: AppStateType) => packs)
-    const {_id} = useSelector(({login}:AppStateType) => login)
-    const {search} = useSelector(({filter}:AppStateType)=> filter)
+    const {cardPacks, currentPage, pageCount, cardPackTotalCount} = useSelector(({packs}: AppStateType) => packs)
+    const {_id} = useSelector(({login}: AppStateType) => login);
+    const {search} = useSelector(({filter}: AppStateType) => filter);
 
 
     useEffect(() => {
-        dispatch(getPacks(_id,currentPage,pageCount))
+        dispatch(getPacks(_id, currentPage, pageCount))
         dispatch(action.setIsAuth(true))
-    }, [_id,currentPage,pageCount])
+    }, [_id, currentPage, pageCount])
 
     const onAddPack = useCallback((name) => {
         dispatch(addPack(name))
-    },[])
+    }, [])
 
     const onDeletePack = useCallback((packId: string) => {
         dispatch(deletePack(packId))
-    },[])
+    }, [])
 
-    const onUpdatePack = useCallback((packId,name) => {
-        dispatch(updatePack(packId,name))
-    },[])
+    const onUpdatePack = useCallback((packId, name) => {
+        dispatch(updatePack(packId, name))
+    }, [])
 
-    const handlePageChange = useCallback((pageNumber:number) => {
+    const handlePageChange = useCallback((pageNumber: number) => {
         console.log(`active page is ${pageNumber}`);
         dispatch(actionPack.setPage(pageNumber))
-    },[])
+    }, [])
 
 
     const filteredPacks = cardPacks.filter(pack => pack.name.toLowerCase().indexOf(search.toLowerCase()) !== -1)
@@ -50,7 +50,7 @@ const Packs = React.memo((props: Props) => {
         <div>
             <SearchFilter/>
             <AddPack onAddPack={onAddPack}/>
-            <Pack onUpdatePack={onUpdatePack}  onDeletePack={onDeletePack} cardPacks={filteredPacks}/>
+            <Table filteredPacks={filteredPacks} onUpdatePack={onUpdatePack} onDeletePack={onDeletePack}/>
             <Pagination totalItemsCount={cardPackTotalCount}
                         onChange={handlePageChange}
                         activePage={currentPage}
